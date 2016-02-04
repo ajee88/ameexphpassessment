be9e4faf792ccceda254b2c42f5e68876b3da4c7
Immanuel Paul <immanuel.paul@ameexusa.com>
2016-02-04 15:38:02 +0530

    Instructions Update

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
+<?php require "http://irwinner.in/ameexphpassessment/secure/dbconfig.php"; ?>
+<!DOCTYPE HTML>
 <html>
 	<head>
-		<title>PHP Assessment</title>
+		<title>Babu | PHP Assessment</title>		
 		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">		
 		<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCeRrMRy9bN6wV8sffTsEfPr98YXG47m1k&signed_in=true"></script>		
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>				
 	</head>
 	<body>
 		<div class="container">
 			<div class="inner-container">
-				<?php include "http://irwinner.in/ameexphpassessment/secure/registerForm.php"; ?>
+				<!-- Here you go with your code -->
 			</div>
 		</div>
 	</body>
