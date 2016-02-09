f9a501ab5b7d6ea8e521426b6bfc86e34f1e16b1
ameex <ameex@ameex>
2016-02-09 14:39:45 +0530

    second commit inserting error

--- babu/test_db.php
+++ babu/test_db.php
@@ -14,15 +14,17 @@ $lastname="testlast";
 $mail="Babumca2016@gmail";
 $phone="8883755110";
 
-//$login_sql = "INSERT INTO ameex_user(name,pass)VALUES('$username','$password')";
-//$login_result = mysqli_query($cn,$login_sql) or die("Error in login Inserting " . mysqli_error($cn));
-//echo json_encode($login_result);			    
-if(!$Reg_result) 
+/*
+$login_sql = "INSERT INTO ameex_user(name,pass)VALUES('$username','$password')";
+$login_result = mysqli_query($cn,$login_sql) or die("Error in login Inserting " . mysqli_error($cn));
+echo json_encode($login_result);			    
+if(!$login_result) 
 {
       die('Could not Enter Login Data: ' . mysql_error()); 
 }
 	echo "<br><b>Entered login data successfully<b>\n";
-   // echo "<br><b>Login Data Deleted successfully<b>\n";
+ echo "<br><b>Login Data Deleted successfully<b>\n"; 
+*/
 
 $Reg_sql = "INSERT INTO ameex_user_profile (first_name,last_name,mail,phone)VALUES('$firstname','$lastname','$mail','$phone')";
 //$Reg_sql="delete from ameex_user_profile where uid=0";
